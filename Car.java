8d5eb04c4426e43e98ca785e54d3b7ff546f8f03
Gloria Dukuzeyesu <gloriadukuzeyesu@ianr-c02tclych3qd.unl.edu>
2022-03-27 22:11:12 -0500

    added my new file

---
+++ Car.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+
+
+
+//Class declaration 
+
+public class Car {
+	public static void main (String[]args) {
+		
+	  	Integer obj = new Integer (100);
+	  	int myNumber = obj.intValue();
+	  	
+	  	System.out.println(myNumber +" "+ obj);
+	  	
+	  	
+	  	
+		
+		
+	
+		
+	}
+			
+}
+		
+			//loop through HashMap
+			
+			//print keySet and values together  
+			
+					//get() method is used to access an element in the ArrayList.Use the index number
+		
+		//System.out.println(make.get(3));
+		
+		//System.out.println(make.size()); checking the size of the loop. Don't use length method in ArrayList
+		
+		
+		
+	
+	
+
